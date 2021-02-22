3a1c8f5d2829d4b63fb435d158ac5268ef9bbaf2
phixyn <phixyn@gmail.com>
2021-02-22 18:22:19 +0000

    App: Refactor App to a class and introduce routing
    Make App a class and set up router.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import Header from './layouts/Header';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Header />
-      </header>
-    </div>
-  );
+import Header from "./layouts/Header";
+import About from './pages/About';
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <p>Sup</p>
+                </React.Fragment>
+              )}
+            />
+
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
