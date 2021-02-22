3dbe8e1eb94e39af8bb4ca99f9bb96ca4563a234
phixyn <phixyn@gmail.com>
2021-02-22 18:27:10 +0000

    Components: Add the TodoItem component
    Add a React component for an individual to-do list item, defining its
    content, style and props.

---
+++ src/components/TodoItem.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export class TodoItem extends React.Component {
+  getStyle = () => {
+    return {
+      background: "#141414",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />
+          &nbsp;
+          {title}
+          <button
+            style={deleteBtnStyle}
+            onClick={this.props.delTodo.bind(this, id)}
+          >
+            X
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
+
+const deleteBtnStyle = {
+  background: "#dd0000",
+  color: "#eee",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
+};
+
+export default TodoItem;
