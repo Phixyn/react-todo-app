4e335b863b9f545104e902ef174ba9f4508345d5
phixyn <phixyn@gmail.com>
2021-02-22 18:39:00 +0000

    App: Fetch and display to-do items from a test API
    Fetch fake to-do list items from the JSON Placeholder testing API. This
    allows us to test the Todos component and verify that it is working.
    Here, Axios is used to perform the request to the test API.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
 
 import Header from "./layouts/Header";
-import About from './pages/About';
+import Todos from "./components/Todos";
+// import AddTodo from "./components/AddTodo";
+import About from "./pages/About";
 
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => this.setState({ todos: res.data }));
+  }
+
+  // Toggle completed state of todo object
+  markComplete = (id) => {
+    return true;
+  };
+
+  // Delete todo item
+  delTodo = (id) => {
+    return true;
+  }
+
   render() {
     return (
       <Router>
@@ -19,7 +43,11 @@ class App extends React.Component {
               path="/"
               render={(props) => (
                 <React.Fragment>
-                  <p>Sup</p>
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
                 </React.Fragment>
               )}
             />
