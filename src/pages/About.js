5170dd747c57150c6d5697af54b42966451d45d9
phixyn <phixyn@gmail.com>
2021-02-23 23:03:33 +0000

    About: Update and style the about page
    Add styling to text and links using TailwindCSS. Also update the content to
    include a link to the GitHub remote repo.

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,24 @@ import React from "react";
 
 function About() {
   return (
-    // "Ghost element", doesn't show up in the DOM.
-    // Useful because all React components need to return
-    // an 'element' of some sort. If you don't want to
-    // polute your DOM, this is very handy.
+    /* React.Fragment does not create any element in the DOM.
+     * Since all components need to return only a single element, this is
+     * useful for cases where we don't want to polute the DOM with an extra
+     * div just for the component. */
     <React.Fragment>
-      <h1>About</h1>
-      <p>This is a simple To-Do list app to demonstrate my React knowledge.</p>
+      <h1 className="text-xl font-bold">About</h1>
+
+      <p className="mt-4">This is a simple to-do list app to demonstrate my <a className="underline font-medium text-blue-500" href="https://reactjs.org/" target="_blank">React</a> knowledge.</p>
+
+      <p className="mt-4">
+        <a
+          className="border-b border-blue-500 font-medium text-blue-500"
+          href="https://github.com/Phixyn/react-todo-app"
+          target="_blank"
+        >
+          View the code on GitHub →
+        </a>
+      </p>
     </React.Fragment>
   );
 }
