f895dfe2e7e626fc44c501072127a632aab97eba
phixyn <phixyn@gmail.com>
2021-02-28 23:12:56 +0000

    AddTodo: Refactor component to use Hooks
    Refactor component into a function based component and use setState hook
    for managing the component's state.

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,45 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-class AddTodo extends React.Component {
-  state = {
-    title: "",
-  };
+function AddTodo(props) {
+  // TODO check react examples to see if they use const or let
+  const [title, setTitle] = useState("");
 
-  onSubmit = (evt) => {
+  const onSubmit = (evt) => {
     // Stop form being submmited to same file and reloading the page
     evt.preventDefault();
-    this.props.addTodo(this.state.title);
-    this.setState({ title: "" });
+
+    // Validate todo text
+    if (!title) {
+      alert("Please add a task description.");
+      return;
+    }
+
+    props.addTodo(title);
+
+    // Clear task text in component state
+    setTitle("");
   };
 
-  // Note evt.target.name will work as long as the property names in
-  // state match the name properties in the HTML (e.g. name="title"
-  // matches title property in state).
-  onChange = (evt) => this.setState({ [evt.target.name]: evt.target.value });
-
-  render() {
-    return (
-      <div className="mt-6 px-3">
-        <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
-          <input
-            type="text"
-            name="title"
-            placeholder="Add task..."
-            value={this.state.title}
-            onChange={this.onChange}
-            className="block w-full px-3 py-2 placeholder-gray-500 bg-white rounded shadow focus:outline-none"
-          />
-          <input
-            type="submit"
-            value="Submit"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="mt-6 px-3">
+      <form onSubmit={onSubmit} style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add task..."
+          value={title}
+          onChange={(evt) => setTitle(evt.target.value)}
+          className="block w-full px-3 py-2 placeholder-gray-500 bg-white rounded shadow focus:outline-none"
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="btn"
+          style={{ flex: "1" }}
+        />
+      </form>
+    </div>
+  );
 }
 
 AddTodo.propTypes = {
