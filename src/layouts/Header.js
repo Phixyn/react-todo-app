cb9b10450e56ab11b08c5c00f9648fbf0fd04d2e
phixyn <phixyn@gmail.com>
2021-02-28 22:40:34 +0000

    Header: Refactor header into a function based component
    Start updating components to be function based components, to take advantage
    of the new Hooks.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,35 +1,25 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="pb-8">
-        <NavLink
-          exact
-          to="/"
-          className="pb-1 mr-4"
-          activeClassName="border-b-2 border-gray-700"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/about"
-          className="pb-1"
-          activeClassName="border-b-2 border-gray-700"
-        >
-          About
-        </NavLink>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="pb-8">
+      <NavLink
+        exact
+        to="/"
+        className="pb-1 mr-4"
+        activeClassName="border-b-2 border-gray-700"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className="pb-1"
+        activeClassName="border-b-2 border-gray-700"
+      >
+        About
+      </NavLink>
+    </header>
+  );
 }
-
-let headerStyle = {
-  background: "rgb(10,10,10)",
-  borderBottom: "1px solid #dd0000",
-  textAlign: "center",
-  padding: "10px",
-};
 
 export default Header;
