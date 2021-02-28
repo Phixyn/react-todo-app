e3b8ea7e4c1fe4d71d3bb7572721683106d432f3
phixyn <phixyn@gmail.com>
2021-02-28 22:29:38 +0000

    About: Use shorthand syntax for fragment
    Also update code style.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,11 @@
-import React from "react";
-
 function About() {
   return (
-    /* React.Fragment does not create any element in the DOM.
-     * Since all components need to return only a single element, this is
-     * useful for cases where we don't want to polute the DOM with an extra
-     * div just for the component. */
-    <React.Fragment>
+    <>
       <h1 className="text-xl font-bold">About</h1>
 
-      <p className="mt-4">This is a simple to-do list app to demonstrate my <a className="underline font-medium text-blue-500" href="https://reactjs.org/" target="_blank">React</a> knowledge.</p>
+      <p className="mt-4">
+        This is a simple to-do list app to demonstrate my <a className="underline font-medium text-blue-500" href="https://reactjs.org/" target="_blank">React</a> knowledge.
+      </p>
 
       <p className="mt-4">
         <a
@@ -20,8 +16,8 @@ function About() {
           View the code on GitHub →
         </a>
       </p>
-    </React.Fragment>
+    </>
   );
-}
+};
 
 export default About;
