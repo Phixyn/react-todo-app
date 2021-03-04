182884fc4d7a1d9487e22f8ad096a513dc994f8a
phixyn <phixyn@gmail.com>
2021-03-04 23:11:02 +0000

    Header: Add hover color to nav links

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="pb-8">
+    <header className="mb-8">
       <NavLink
         exact
         to="/"
-        className="pb-1 mr-4"
+        className="pb-1 mr-4 transition duration-500 ease-in-out hover:text-gray-700"
         activeClassName="border-b-2 border-pink-600"
       >
         Home
       </NavLink>
       <NavLink
         to="/about"
-        className="pb-1"
+        className="pb-1 transition duration-500 ease-in-out hover:text-gray-700"
         activeClassName="border-b-2 border-pink-600"
       >
         About
