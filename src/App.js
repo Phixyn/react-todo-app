bd06a45d0ba9e4b7124cea6114743638d7393b84
phixyn <phixyn@gmail.com>
2021-03-07 18:21:28 +0000

    Todos: Move all logic about todos to Todos component and rename it
    Put everything related to todo list in the TodoList component.

--- src/App.js
+++ src/App.js
@@ -1,87 +1,17 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
-// import axios from "axios"; // TODO remove from package.json
 import Header from "./components/Header";
-import TodosHeader from "./components/TodosHeader";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 import About from "./pages/About";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      completed: true,
-    },
-    {
-      id: 2,
-      title: "Meeting at School",
-      completed: false,
-    },
-    {
-      id: 3,
-      title: "Food Shopping",
-      completed: false,
-    },
-  ]);
-  // const [todos, setTodos] = useState([]);
-
-  // Add a new todo item
-  const addTodo = (title) => {
-    let newTodo = {
-      id: uuidv4(),
-      title, // new in ES6: same as title: title
-      completed: false,
-    };
-
-    // [...] = spread operator (copy items)
-    // Used because we can't (and shouldn't) change state values directly
-    setTodos([...todos, newTodo]);
-  };
-
-  // Delete a todo item
-  const delTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  // Toggle completed state of todo item
-  const markComplete = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
   return (
     <div id="app" className="bg-gray-300 h-screen pt-8">
       <Router>
         <div className="max-w-md mx-auto">
           <Header />
-
           <section>
             <Switch>
-              <Route
-                exact
-                path="/"
-                render={(props) => (
-                  <div className="max-w-md pb-6 mx-auto bg-gray-200 rounded shadow-lg">
-                    <TodosHeader />
-
-                    <AddTodo addTodo={addTodo} />
-
-                    <Todos
-                      todos={todos}
-                      markComplete={markComplete}
-                      delTodo={delTodo}
-                    />
-                  </div>
-                )}
-              />
-
+              <Route exact path="/" component={TodoList} />
               <Route path="/about" component={About} />
             </Switch>
           </section>
---
+++ src/components/TodoList.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from "prop-types";
+import TodosHeader from "./TodosHeader";
+import AddTodo from "./AddTodo";
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Feed the cat",
+      day: "Feb 5th at 2:30pm",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Meeting at school",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Grocery shopping",
+      completed: false,
+    },
+  ]);
+  // const [todos, setTodos] = useState([]);
+
+  // Add a new todo item
+  const addTodo = (title) => {
+    let newTodo = {
+      id: uuidv4(),
+      title, // new in ES6: same as title: title
+      completed: false,
+    };
+
+    // [...] = spread operator (copy items)
+    // Used because we can't (and shouldn't) change state values directly
+    setTodos([...todos, newTodo]);
+  };
+
+  // Delete a todo item
+  const delTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  // Toggle completed state of todo item
+  const markComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="max-w-md pb-6 mx-auto bg-gray-200 rounded shadow-lg">
+      <TodosHeader />
+
+      <AddTodo addTodo={addTodo} />
+
+      <div className="px-4 mt-6 max-h-screen overflow-auto">
+        {todos.length > 0 ? (
+          // If there are todo items, show them in a list
+          <ul className="mt-4">
+            {todos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                markComplete={markComplete}
+                delTodo={delTodo}
+              />
+            ))}
+          </ul>
+        ) : (
+          // No todo items, all caught up
+          <p
+            className="px-3 pb-6 mt-16 text-lg text-center text-gray-500"
+            data-testid="empty-todos"
+          >
+            You're all caught up!
+          </p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
+
+export default TodoList;
