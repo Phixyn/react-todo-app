a77a661b871c1a8bfc6708497a3e11548ff50ff1
phixyn <phixyn@gmail.com>
2021-03-10 23:21:34 +0000

    TodosFooter: Vertically center text in footer

--- src/components/TodosFooter.js
+++ src/components/TodosFooter.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function TodosFooter(props) {
   return (
-    <div className="px-4 py-3 h-14 bg-gray-300 border-t border-gray-400 flex flex-row flex-wrap text-gray-600">
+    <div className="px-4 h-12 text-sm bg-gray-300 border-t border-gray-400 flex flex-wrap items-center text-gray-600">
       <p className="flex-1 order-1">{props.totalTasks} tasks</p>
       <p className="flex-1 order-2 text-center">{props.doneTasks} complete</p>
       <p className="flex-1 order-last text-right">
