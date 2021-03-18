cdae14e34c1ad451a908bc2252bcb1f4b4d3135c
phixyn <phixyn@gmail.com>
2021-03-18 18:22:27 +0000

    TodoItem: Use Tailwind classes for styling, even conditionally
    Previously, some conditional styles were being applied with the style
    attribute. Now we use Tailwind classes for these instead.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,17 @@ import { FaTrashAlt } from "react-icons/fa";
 import PropTypes from "prop-types";
 
 function TodoItem(props) {
-  // TODO: convert to TailwindCSS classnames?
-  const getStyle = () => {
-    return {
-      textDecoration: props.todo.completed ? "line-through" : "none",
-      color: props.todo.completed ? "rgb(219, 39, 119)" : "#121212"
-    };
-  };
+  const textDecorationClass = props.todo.completed
+    ? "line-through"
+    : "no-underline";
+  const textColorClass = props.todo.completed
+    ? "text-pink-600"
+    : "text-gray-800";
 
   return (
-    <li style={getStyle()} className="flex items-center space-x-1 py-2.5 px-2.5 border-b border-gray-300 transition duration-500 ease-in">
+    <li
+      className={`flex items-center space-x-1 py-2.5 px-2.5 border-b border-gray-300 transition duration-500 ease-in ${textDecorationClass} ${textColorClass}`}
+    >
       <input
         name="completed-checkbox"
         type="checkbox"
@@ -21,7 +22,9 @@ function TodoItem(props) {
         onChange={() => props.markComplete(props.todo.id)}
         data-testid="task-completed-checkbox"
       />
-      <span className="flex-1 px-2 min-w-0 max-w-full break-words">{props.todo.title}</span>
+      <span className="flex-1 px-2 min-w-0 break-words">
+        {props.todo.title}
+      </span>
       <button
         onClick={() => props.delTodo(props.todo.id)}
         data-testid="delete-task-btn"
