f8bce20079ac5ba31b7a17282f2713b18de10f49
phixyn <phixyn@gmail.com>
2021-03-18 14:45:22 +0000

    Cypress: Implement addTask command and refactor TodosFooter spec to use it
    Implement a Cypress command that adds a new task to the to-do list, optionally
    marking it as complete. Refactor the tests for the TodosFooter component to
    use this, making them more readable and easier to understand.

--- cypress/integration/TodosFooter.spec.js
+++ cypress/integration/TodosFooter.spec.js
@@ -27,22 +27,14 @@ describe("todo list footer", () => {
   });
 
   it("should update number of total tasks", () => {
-    let task = random.words();
-
-    // Check number of total tasks
     cy.getByTestId("total-tasks-count").should("have.text", "0 tasks");
-
-    // Add new task
-    cy.getByTestId("task-input-field").type(task);
-    cy.getByTestId("task-submit-btn").click();
-
-    // Check number of total tasks again
+    cy.addTask(random.words());
     cy.getByTestId("total-tasks-count").should("have.text", "1 tasks");
 
     // Delete task
     cy.getByTestId("todos-list")
-      .find("li")
-      .contains(task)
+      .get("li")
+      .last()
       .getByTestId("delete-task-btn")
       .click();
 
@@ -50,43 +42,20 @@ describe("todo list footer", () => {
   });
 
   it("should update number of complete tasks", () => {
-    let task = random.words();
-
-    cy.getByTestId("completed-tasks-count").should(
-      "have.text",
-      "0 complete"
-    );
-
-    cy.getByTestId("task-input-field").type(task);
-    cy.getByTestId("task-submit-btn").click();
-
-    // Mark task as complete
-    cy.getByTestId("todos-list")
-      .find("li")
-      .contains(task)
-      .getByTestId("task-completed-checkbox")
-      .click();
-
-    cy.getByTestId("completed-tasks-count").should(
-      "have.text",
-      "1 complete"
-    );
+    cy.getByTestId("completed-tasks-count").should("have.text", "0 complete");
+    cy.addTask(random.words(), true);
+    cy.getByTestId("completed-tasks-count").should("have.text", "1 complete");
   });
 
   it("should update number of open tasks", () => {
-    let task = random.words();
-
     cy.getByTestId("open-tasks-count").should("have.text", "0 open");
-
-    cy.getByTestId("task-input-field").type(task);
-    cy.getByTestId("task-submit-btn").click();
-
+    cy.addTask(random.words());
     cy.getByTestId("open-tasks-count").should("have.text", "1 open");
 
     // Mark task as complete
     cy.getByTestId("todos-list")
-      .find("li")
-      .contains(task)
+      .get("li")
+      .last()
       .getByTestId("task-completed-checkbox")
       .click();
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,3 +10,17 @@
 Cypress.Commands.add("getByTestId", (testid, ...args) => {
   return cy.get(`[data-testid=${testid}]`, ...args);
 });
+
+Cypress.Commands.add("addTask", (task, isComplete = false) => {
+  cy.getByTestId("task-input-field").type(task);
+  cy.getByTestId("task-submit-btn").click();
+
+  if (isComplete) {
+    // Mark task as complete, assuming it's the latest one on the list
+    cy.getByTestId("todos-list")
+      .get("li")
+      .last()
+      .getByTestId("task-completed-checkbox")
+      .click();
+  }
+});
