acb2c6eda3d6dd271a92681be484c29a965771d0
phixyn <phixyn@gmail.com>
2021-03-23 23:35:56 +0000

    TodosHeader: Display 'st', 'nd', 'rd' or 'th' depending on day of month
    Update the date display to correct show the right suffix for the current
    day of the month, instead of always showing 'th' for every day.
    
    For example, before this commit, it showed 2th instead of 2nd.

--- src/components/TodosHeader.js
+++ src/components/TodosHeader.js
@@ -26,12 +26,26 @@ function TodosHeader() {
     "December",
   ][date.getMonth()];
 
+  // TODO check if there are more elegant solutions than this
+  //    but tbfh, this was pretty good problem solving xD
+  const getDaySuffix = (calendarDay) => {
+    switch (calendarDay % 10) {
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
+    }
+  };
+
   return (
     <header
       className="bg-todo-header bg-cover bg-center border-b-1 border-gray-300 px-4 py-6"
     >
-      {/* TODO set 'st, nd, rd, th' programmatically */}
-      <h1 className="text-2xl text-white">{`${weekDay}, ${day}th`}</h1>
+      <h1 className="text-2xl text-white">{`${weekDay}, ${day}${getDaySuffix(day)}`}</h1>
       <p className="pt-1 text-lg text-gray-100">{month}</p>
     </header>
   );
