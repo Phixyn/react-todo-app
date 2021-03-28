551543537045560624ef364d079c5903831e011f
phixyn <phixyn@gmail.com>
2021-03-28 22:49:28 +0100

    Tests: Add UI tests for TodoItem component

---
+++ cypress/integration/TodoItem.spec.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+import { random } from "faker";
+
+describe("todo list", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.waitForReact(5000, "#root");
+  });
+
+  it("displays the correct task description", () => {
+    let task = random.words();
+
+    cy.addTask(task);
+    cy.getByTestId("todos-list")
+      .find("li")
+      .first()
+      .should("have.text", task);
+  });
+
+  it("can be marked as complete", () => {
+    cy.addTask(random.words());
+
+    // "Simple" way
+    // cy.getByTestId("todos-list")
+    //   .find("li")
+    //   .first()
+    //   .findByTestId("task-completed-checkbox")
+    //   .click()
+    //   .should("be.checked");
+
+    // "Fancy" way with closure and alias
+    cy.getByTestId("todos-list")
+      .find("li")
+      .first()
+      .findByTestId("task-completed-checkbox")
+      .then(($checkbox) => {
+        $checkbox.click();
+        expect($checkbox).to.be.checked;
+      });
+  });
+
+  it("can be deleted", () => {
+    let task = random.words();
+
+    // Add a couple of tasks to work with and make test a bit "realistic"
+    cy.addTask(task);
+    cy.addTask(random.words());
+
+    // All of the below can use closures, but at the cost of readability
+    cy.getByTestId("todos-list")
+      .contains(task)
+      .should("exist");
+
+    // Delete the first task
+    cy.getByTestId("todos-list")
+      .find("li")
+      .contains(task)
+      .next()
+      .should("have.attr", "data-testid", "delete-task-btn")
+      .click();
+
+    // Assert deletion
+    cy.getByTestId("todos-list", {
+      timeout: 0,
+    }).contains(task)
+      .should("not.exist");
+  });
+});
