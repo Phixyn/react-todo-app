5c95c0371b3c0f03039908ad11970bb0fdd72785
Alpeche Pancha <alpeche.pancha@dataformlab.com>
2025-07-23 10:39:10 +0100

    Clean up generated code
    Remove unused code generated by Vite.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import AppFooter from "./components/AppFooter.jsx";
 import TodosList from "./components/TodosList.jsx";
 import About from "./pages/About.jsx";
 
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div id="app" className="flex flex-col container max-w-md mx-auto md:pt-8">
       <Router>
