17b5b491f3edffda7185c6b23203f64e51c5577e
Alpeche Pancha <alpeche.pancha@dataformlab.com>
2025-07-24 15:31:09 +0100

    Clean up Cypress support files
    Remove auto-generated and verbose comments. Information about
    each file can be found in the Cypress documentation instead.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,12 +1,4 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This file can be used to create various
-// custom commands and overwrite existing ones.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
 
 /**
  * Queries the entire DOM for an element with the given data-testid value.
