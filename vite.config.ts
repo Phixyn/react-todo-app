189ee2456b8f5e72bb61c0049f52218b680780c3
Phixyn <phixyn@gmail.com>
2025-07-25 16:44:10 +0100

    Add coverage report for Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,5 +13,8 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'v8'
+    },
   },
 })
