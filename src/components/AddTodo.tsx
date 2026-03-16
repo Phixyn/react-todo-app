b8685e79255af809101b2e2790454f7e3285679d
Phixyn <phixyn@gmail.com>
2026-03-16 18:22:05 +0200

    Bring back the original component files

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,25 +1,36 @@
-import { useState } from 'react';
-import { FaPlusSquare } from 'react-icons/fa';
-import PropTypes from 'prop-types';
+import { useContext, useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
 
-function AddTodo(props) {
-	const [title, setTitle] = useState('');
+import { GlobalContext } from "../context/GlobalState";
 
-	const onSubmit = (evt) => {
-		// Stop form being submmited to same file and reloading the page
-		evt.preventDefault();
+export default function AddTodo() {
+  const [title, setTitle] = useState("");
 
-		// Validate todo text
-		if (!title) {
-			alert('Please add a task description.');
-			return;
-		}
+  const { addTodo } = useContext(GlobalContext);
 
-		props.addTodo(title);
+  const onSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    // Stop form being submitted to same file and reloading the page
+    evt.preventDefault();
 
-		// Clear task text in component state
-		setTitle('');
-	};
+    // Validate todo text
+    if (!title) {
+      // TODO Replace with toast
+      alert("Please add a task description.");
+      return;
+    }
+
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+
+    addTodo(newTodo);
+
+    // Clear task text in component state
+    setTitle("");
+  };
 
   return (
     <div className="mx-4 mt-6">
@@ -48,35 +59,3 @@ function AddTodo(props) {
     </div>
   );
 }
-	return (
-		<div className="mx-4 mt-6">
-			<form
-				onSubmit={onSubmit}
-				className="flex items-center transition duration-500 ease-in-out py-2 border-b-2 border-gray-300 focus-within:border-b-2 focus-within:border-pink-600"
-			>
-				<input
-					name="task-title"
-					type="text"
-					placeholder="Add task..."
-					value={title}
-					onChange={(evt) => setTitle(evt.target.value)}
-					className="flex-1 px-2.5 bg-gray-200 placeholder-gray-500 focus:outline-none"
-					data-testid="task-input-field"
-				/>
-				<button
-					type="submit"
-					className="transition duration-200 ease-in-out text-gray-400 focus:outline-none hover:text-pink-500 text-lg px-2 cursor-pointer"
-					data-testid="task-submit-btn"
-				>
-					<FaPlusSquare />
-				</button>
-			</form>
-		</div>
-	);
-}
-
-AddTodo.propTypes = {
-	addTodo: PropTypes.func.isRequired
-};
-
-export default AddTodo;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,52 +1,40 @@
-import { FaTrashAlt, FaEdit } from 'react-icons/fa';
-import PropTypes from 'prop-types';
+import { useContext } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
-function TodoItem(props) {
-	let textDecorationClass = props.todo.completed
-		? 'line-through'
-		: 'no-underline';
-	let textColorClass = props.todo.completed ? 'text-pink-600' : 'text-gray-800';
+import { GlobalContext } from "../context/GlobalState";
+import type { TodoItemType } from "./TodosList";
 
-	return (
-		<li
-			className={`flex items-center space-x-1 py-2.5 px-2.5 border-b border-gray-300 transition duration-300 ease-in ${textDecorationClass} ${textColorClass}`}
-			data-testid="todo-item"
-		>
-			<input
-				name="completed-checkbox"
-				type="checkbox"
-				className="form-checkbox rounded text-pink-600 shadow-none focus:shadow-none focus:ring-0 focus:ring-offset-0 focus:outline-none"
-				checked={props.todo.completed}
-				value={props.todo.completed}
-				onChange={() => props.markComplete(props.todo.id)}
-				data-testid="task-completed-checkbox"
-			/>
-			<span className="flex-1 px-2 min-w-0 break-words">
-				{props.todo.title}
-			</span>
-			<button
-				style={{ marginRight: 5 }}
-				onClick={() => props.updateTodo(props.todo.id)}
-				className="transition duration-200 ease-in-out text-gray-400 hover:text-pink-500 focus:outline-none"
-				data-testid="delete-task-btn"
-			>
-				<FaEdit />
-			</button>
-			<button
-				onClick={() => props.delTodo(props.todo.id)}
-				className="transition duration-200 ease-in-out text-gray-400 hover:text-pink-500 focus:outline-none"
-				data-testid="delete-task-btn"
-			>
-				<FaTrashAlt />
-			</button>
-		</li>
-	);
+interface TodoItemProps {
+  todo: TodoItemType;
 }
 
-TodoItem.propTypes = {
-	todo: PropTypes.object.isRequired,
-	markComplete: PropTypes.func.isRequired,
-	delTodo: PropTypes.func.isRequired
-};
+export default function TodoItem({ todo }: TodoItemProps) {
+  const { toggleTodoComplete, deleteTodo } = useContext(GlobalContext);
 
-export default TodoItem;
+  const textDecorationClass = todo.completed ? "line-through" : "no-underline";
+  const textColorClass = todo.completed ? "text-pink-600" : "text-gray-800";
+
+  return (
+    <li
+      className={`flex items-center space-x-1 py-2.5 px-2.5 border-b border-gray-300 transition duration-300 ease-in ${textDecorationClass} ${textColorClass}`}
+      data-testid="todo-item"
+    >
+      <input
+        name="completed-checkbox"
+        type="checkbox"
+        className="form-checkbox rounded text-pink-600 shadow-none focus:shadow-none focus:ring-0 focus:ring-offset-0 focus:outline-none"
+        checked={todo.completed}
+        onChange={() => toggleTodoComplete(todo.id)}
+        data-testid="task-completed-checkbox"
+      />
+      <span className="flex-1 px-2 min-w-0 break-words">{todo.title}</span>
+      <button
+        onClick={() => deleteTodo(todo.id)}
+        className="transition duration-200 ease-in-out text-gray-400 hover:text-pink-500 focus:outline-none"
+        data-testid="delete-task-btn"
+      >
+        <FaTrashAlt />
+      </button>
+    </li>
+  );
+}
