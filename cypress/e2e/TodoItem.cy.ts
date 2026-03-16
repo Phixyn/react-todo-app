c685f129446f867e4cde4d84cd7af45e3ce0a1e4
Alpeche Pancha <phixyn@gmail.com>
2026-03-16 16:59:19 +0000

    Update Cypress tests for TodoItem component
    Fix failing delete test and add a new test for editing a task.

--- cypress/e2e/TodoItem.cy.ts
+++ cypress/e2e/TodoItem.cy.ts
@@ -37,6 +37,34 @@ describe("todo list", () => {
       });
   });
 
+it("can be edited", () => {
+    const originalTask = faker.word.words();
+    const editedTask = faker.word.words();
+
+    cy.addTask(originalTask);
+
+    // Click the edit button
+    cy.contains("li", originalTask).findByTestId("edit-task-btn").click();
+
+    // The input should be visible and focused
+    cy.contains("li", originalTask)
+      .find("input[type='text']")
+      .should("have.value", originalTask);
+
+    // Clear and type new value
+    cy.contains("li", originalTask)
+      .find("input[type='text']")
+      .clear()
+      .type(editedTask);
+
+    // Click save
+    cy.contains("li", editedTask).findByTestId("save-task-btn").click();
+
+    // Assert the task was updated
+    cy.getByTestId("todos-list").contains(editedTask).should("exist");
+    cy.getByTestId("todos-list").contains(originalTask).should("not.exist");
+  });
+
   it("can be deleted", () => {
     const task = faker.word.words();
 
@@ -47,13 +75,8 @@ describe("todo list", () => {
     // All of the below can use closures, but at the cost of readability
     cy.getByTestId("todos-list").contains(task).should("exist");
 
-    // Delete the first task
-    cy.getByTestId("todos-list")
-      .find("li")
-      .contains(task)
-      .next()
-      .should("have.attr", "data-testid", "delete-task-btn")
-      .click();
+    // Delete the task
+    cy.contains("li", task).findByTestId("delete-task-btn").click();
 
     // Assert deletion
     cy.getByTestId("todos-list", {
